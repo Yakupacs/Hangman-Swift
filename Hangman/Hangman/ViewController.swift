ed0309d74e0b72ec7ca4eee387d2af62766fd02f
Yusuf Açış <yusufacis@Yusuf-MacBook-Pro.local>
2022-09-04 02:15:08 +0300

    Updated.

---
+++ Hangman/Hangman/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by Yakup Acis on 8/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameTextfield: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(keywordClose))
+        view.addGestureRecognizer(gestureRecognizer)
+    }
+    @IBAction func startFunc(_ sender: Any) {
+        if (nameTextfield.text != ""){
+            performSegue(withIdentifier: "toGame", sender: nil)
+        }
+        else{
+            let warning = UIAlertController(title: "Warning!", message: "Please enter a name.", preferredStyle: .alert)
+            let okey = UIAlertAction(title: "Okey", style: UIAlertAction.Style.default, handler: { (UIAlertAction) in
+            })
+            warning.addAction(okey)
+            self.present(warning, animated: true, completion: nil)
+        }
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toGame"{
+            let destVC = segue.destination as! ViewControllerGame
+            destVC.name = nameTextfield.text!
+        }
+    }
+    @objc func keywordClose(){
+            view.endEditing(true)
+        }
+}
+
